64252d310b009cc6ee3c9ca8219cb3540d4466f2
kate smith <kateesmith4994@gmail.com>
2020-11-06 12:06:09 +0000

    bank statement returns everything from all tests

--- bank.go
+++ bank.go
@@ -11,8 +11,6 @@ type Account struct {
 
 var m = make(map[string]int)
 
-// type AccountErr string
-
 var (
 	NotEnoughMoneyInAccount = errors.New("You do not have enough money in your account.")
 )
@@ -24,25 +22,29 @@ func (a *Account) Balance() int {
 func (a *Account) Add(deposit int) {
 	a.Amount += deposit
 	total := a.Amount
-	comment := ""
-	comment += "You added " + strconv.Itoa(deposit) + ". "
-	comment += "Balance: " + strconv.Itoa(a.Amount) + ". \n"
+	addComment := ""
+	addComment += "You added " + strconv.Itoa(deposit) + ". "
+	addComment += "Balance: " + strconv.Itoa(a.Amount) + ". \n"
 
-	m[comment] = total
+	m[addComment] = total
 }
 
-// func (a *Account) Withdraw(value int) error {
-// 	if value > a.Amount {
-// 		return NotEnoughMoneyInAccount
-// 	} else {
-// 		a.Amount -= value
+func (a *Account) Withdraw(value int) error {
+	if value > a.Amount {
+		return NotEnoughMoneyInAccount
+	} else {
+		a.Amount -= value
+		total := a.Amount
 
-// 		a.Statement += "You withdrew " + strconv.Itoa(value) + ". "
-// 		a.Statement += "Balance: " + strconv.Itoa(a.Amount) + ". \n"
-// 		return nil
-// 	}
+		withdrawComment := ""
+		withdrawComment += "You withdrew " + strconv.Itoa(value) + ". "
+		withdrawComment += "Balance: " + strconv.Itoa(a.Amount) + ". \n"
 
-// }
+		m[withdrawComment] = total
+		return nil
+	}
+
+}
 
 func (a *Account) BankStatement() string {
 	returnStatement := ""
--- bank_test.go
+++ bank_test.go
@@ -35,85 +35,69 @@ func assertError(t *testing.T, got, want error) {
 }
 
 func TestCanCheckBalance(t *testing.T) {
-	account := Account{10}
-	got := account.Balance()
+	account1 := Account{10}
+	got := account1.Balance()
 	want := 10
 
 	assertValues(t, got, want)
 }
 
-// func TestCanAddMoney(t *testing.T) {
-// 	account := Account{100, ""}
+func TestCanAddMoney(t *testing.T) {
 
-// 	t.Run("one addition", func(t *testing.T) {
-// 		account.Add(10)
-// 		got := account.Balance()
-// 		want := 110
+	t.Run("multiple additions", func(t *testing.T) {
+		account2 := Account{100}
+		account2.Add(20)
+		account2.Add(12)
+		account2.Add(8)
 
-// 		assertValues(t, got, want)
-// 	})
+		got := account2.Balance()
+		want := 140
 
-// 	t.Run("multiple additions", func(t *testing.T) {
-// 		account.Add(20)
-// 		account.Add(12)
-// 		account.Add(8)
+		assertValues(t, got, want)
+	})
+}
 
-// 		got := account.Balance()
-// 		want := 150
+func TestCanWithdrawMoney(t *testing.T) {
 
-// 		assertValues(t, got, want)
-// 	})
-// }
+	t.Run("multiple withdawl", func(t *testing.T) {
+		account3 := Account{650}
+		account3.Withdraw(10)
+		account3.Withdraw(1)
+		account3.Withdraw(2)
 
-// func TestCanWithdrawMoney(t *testing.T) {
-// 	account := Account{90, ""}
+		got := account3.Balance()
+		want := 637
 
-// 	t.Run("single withdrawl", func(t *testing.T) {
-// 		account.Withdraw(20)
-// 		got := account.Balance()
-// 		want := 70
-
-// 		assertValues(t, got, want)
-// 	})
-
-// 	t.Run("multiple withdawl", func(t *testing.T) {
-// 		account.Withdraw(10)
-// 		account.Withdraw(1)
-// 		account.Withdraw(2)
-
-// 		got := account.Balance()
-// 		want := 57
-
-// 		assertValues(t, got, want)
-// 	})
-
-// 	t.Run("can't withdraw money if not enough in account", func(t *testing.T) {
-// 		account := Account{20, ""}
-// 		err := account.Withdraw(40)
-
-// 		assertError(t, err, NotEnoughMoneyInAccount)
-// 	})
-// }
-
-func TestBankStatementPrints1Interactions(t *testing.T) {
-	t.Run("Single statement", func(t *testing.T) {
-		account := Account{50}
-		account.Add(10)
-		got := account.BankStatement()
-		want := "You added 10. Balance: 60. \n"
-
-		assertStrings(t, got, want)
+		assertValues(t, got, want)
 	})
 
-	// 	t.Run("multiple statement", func(t *testing.T) {
-	// 		account := Account{20, ""}
-	// 		account.Add(1)
-	// 		account.Withdraw(2)
-	// 		account.Add(5)
-	// 		account.Withdraw(1)
-	// 		got := account.BankStatement()
-	// 		want := "You added 1. Balance: 21. \nYou withdrew 2. Balance: 19. \nYou added 5. Balance: 24. \nYou withdrew 1. Balance: 23. \n"
+	t.Run("can't withdraw money if not enough in account", func(t *testing.T) {
+		account4 := Account{20}
+		err := account4.Withdraw(40)
+
+		assertError(t, err, NotEnoughMoneyInAccount)
+	})
+}
+
+func TestBankStatementPrints1Interactions(t *testing.T) {
+	// 	t.Run("Single statement", func(t *testing.T) {
+	// 		account6 := Account{50}
+	// 		account6.Add(10)
+	// 		got := account6.BankStatement()
+	// 		want := "You added 10. Balance: 60. \n"
 
 	// 		assertStrings(t, got, want)
 	// 	})
+
+	t.Run("multiple statement", func(t *testing.T) {
+		account := Account{20}
+		account.Add(1)
+		account.Withdraw(2)
+		account.Add(5)
+		account.Withdraw(1)
+		got := account.BankStatement()
+		want := "You added 1. Balance: 21. \nYou withdrew 2. Balance: 19. \nYou added 5. Balance: 24. \nYou withdrew 1. Balance: 23. \n"
+
+		assertStrings(t, got, want)
+	})
 }
