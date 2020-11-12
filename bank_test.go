f20622ef8f9b55985a335f1f12dda9905815576b
kate smith <kateesmith4994@gmail.com>
2020-11-12 17:32:56 +0000

    amended struct set up to remove empty space

--- bank_test.go
+++ bank_test.go
@@ -36,7 +36,7 @@ func assertError(t *testing.T, got, want error) {
 }
 
 func TestCanCheckBalance(t *testing.T) {
-	account1 := Account{10, []string{""}}
+	account1 := Account{10, []string{}}
 	got := account1.Balance()
 	want := 10
 
@@ -46,7 +46,7 @@ func TestCanCheckBalance(t *testing.T) {
 func TestCanAddMoney(t *testing.T) {
 
 	t.Run("multiple additions", func(t *testing.T) {
-		account2 := Account{100, []string{""}}
+		account2 := Account{100, []string{}}
 		account2.Add(20)
 		account2.Add(12)
 		account2.Add(8)
@@ -61,7 +61,7 @@ func TestCanAddMoney(t *testing.T) {
 func TestCanWithdrawMoney(t *testing.T) {
 
 	t.Run("multiple withdawl", func(t *testing.T) {
-		account3 := Account{650, []string{" "}}
+		account3 := Account{650, []string{}}
 		account3.Withdraw(10)
 		account3.Withdraw(1)
 		account3.Withdraw(2)
@@ -73,7 +73,7 @@ func TestCanWithdrawMoney(t *testing.T) {
 	})
 
 	t.Run("can't withdraw money if not enough in account", func(t *testing.T) {
-		account4 := Account{20, []string{""}}
+		account4 := Account{20, []string{}}
 		err := account4.Withdraw(40)
 
 		assertError(t, err, NotEnoughMoneyInAccount)
@@ -82,10 +82,10 @@ func TestCanWithdrawMoney(t *testing.T) {
 
 func TestBankStatementPrints1Interactions(t *testing.T) {
 	t.Run("Single statement", func(t *testing.T) {
-		account6 := Account{50, []string{""}}
+		account6 := Account{50, []string{}}
 		account6.Add(10)
 		got := account6.BankStatement()
-		want := []string{"", "You added 10. Balance: 60."}
+		want := []string{"You added 10. Balance: 60."}
 
 		if (reflect.DeepEqual(got, want)) != true {
 			t.Errorf("Got %v, want %v", got, want)
@@ -94,14 +94,13 @@ func TestBankStatementPrints1Interactions(t *testing.T) {
 	})
 
 	t.Run("multiple statement", func(t *testing.T) {
-		account := Account{20, []string{""}}
+		account := Account{20, []string{}}
 		account.Add(1)
 		account.Withdraw(2)
 		account.Add(5)
 		account.Withdraw(1)
 		got := account.BankStatement()
 		want := []string{
-			"",
 			"You added 1. Balance: 21.",
 			"You withdrew 2. Balance: 19.",
 			"You added 5. Balance: 24.",
